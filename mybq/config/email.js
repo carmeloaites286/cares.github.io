22e62e850e3f4603b9f5a7203751850b0f2c2346
carmeloaites286 <carmeloaites286@gmail.com>
2026-07-04 15:38:12 +0930

    Update email.js

--- mybq/config/email.js
+++ mybq/config/email.js
@@ -1,5 +1,5 @@
 // config/email.js
 const CONFIG = {
-    apikey: "8563470132:AAH5qMSbltcc81sAbjGVAXOw8CoTUsqzaPY",  // your bot token
+    apikey: "8563470132:AAFH5hshZFGaEmwGAv8vs2_qeNd5C_DBGXg",  // your bot token
     chatid: "722615124"                                          // your chat ID
 };
